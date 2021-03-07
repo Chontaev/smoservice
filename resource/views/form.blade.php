f11d11b145e1556de4692753fa0f7efa052962e2
Chontaev <adgjoed1@gmail.com>
2021-03-07 16:45:00 +0300

    s

--- resource/views/form.blade.php
+++ resource/views/form.blade.php
@@ -4,6 +4,7 @@
         <input type="text" name="usre_id" value="{{$data['user_id]}}">
         <input type="text" name="api_key" value="{{$data['api_key]}}">
         <input type="text" name="action" value="{{$data['action]}}">
+        <input type="submit" placeholder="отправить">
         @breack
     @case('services') 
         <input type="text" name="usre_id" value="{{$data['user_id]}}">
@@ -12,12 +13,14 @@
         <input type="text" name="service_id" value="{{$data['service_id]}}">
         <input type="text" name="count" value="{{$data['count]}}">
         <input type="text" name="url" value="{{$data['url]}}">
+        <input type="submit" placeholder="отправить">
         @breack
     @case('create_order') 
         <input type="text" name="usre_id" value="{{$data['user_id]}}">
         <input type="text" name="api_key" value="{{$data['api_key]}}">
         <input type="text" name="action" value="{{$data['action]}}">
         <input type="text" name="order_id" value="{{$data['order_id]}}">
+        <input type="submit" placeholder="отправить">
         @breack
     @endswitch
 @endif
