3df5a99e0c13e2df8c59b087d4ad5418e4f14f4e
Chontaev <adgjoed1@gmail.com>
2021-03-09 01:03:38 +0300

    add exampleController

--- resource/views/form.blade.php
+++ resource/views/form.blade.php
@@ -1,30 +1,35 @@
 @if(isset($data))
 <div class="container">
-    @if($data['action']=='balance' || $data['action']=='services')
-    <form action="https://smoservice.media/api/" method="post">
-        <input type="hidden" name="user_id" value="{{$data['user_id']}}">
-        <input type="hidden" name="api_key" value="{{$data['api_key']}}">
-        <input type="hidden" name="action" value="{{$data['action']}}">
-        <input type="submit" placeholder="перейти">
-    </form>
-    @elseif($data['action']=='create_order')
-        <form action="https://smoservice.media/api/" method="post">
-            <input type="hidden" name="user_id" value="{{$data['user_id']}}">
-            <input type="hidden" name="api_key" value="{{$data['api_key']}}">
-            <input type="hidden" name="action" value="{{$data['action']}}">
-            <input type="hidden" name="service_id" value="{{$data['service_id']}}">
-            <input type="hidden" name="count" value="{{$data['count']}}">
-            <input type="hidden" name="url" value="{{$data['url']}}">
-            <input type="submit" placeholder="перейти">
-        </form>
-    @elseif($data['action']=='check_order') 
-        <form action="https://smoservice.media/api/" method="post">
-            <input type="hidden" name="user_id" value="{{$data['user_id']}}">
-            <input type="hidden" name="api_key" value="{{$data['api_key']}}">
-            <input type="hidden" name="action" value="{{$data['action']}}">
-            <input type="hidden" name="order_id" value="{{$data['order_id']}}">
-            <button type="submit">отправить</button>
-        </form>
-    @endif
+    @switch($data['action'])
+        @case('balance' || 'services')
+            <form action="https://smoservice.media/api/" method="post">
+                <input type="hidden" name="user_id" value="{{$data['user_id']}}">
+                <input type="hidden" name="api_key" value="{{$data['api_key']}}">
+                <input type="hidden" name="action" value="{{$data['action']}}">
+                <input type="submit" placeholder="перейти">
+            </form>
+            @break
+        @case('create_order')
+            <form action="https://smoservice.media/api/" method="post">
+                <input type="hidden" name="user_id" value="{{$data['user_id']}}">
+                <input type="hidden" name="api_key" value="{{$data['api_key']}}">
+                <input type="hidden" name="action" value="{{$data['action']}}">
+                <input type="hidden" name="service_id" value="{{$data['service_id']}}">
+                <input type="hidden" name="count" value="{{$data['count']}}">
+                <input type="hidden" name="url" value="{{$data['url']}}">
+                <input type="submit" placeholder="перейти">
+            </form>
+            @break
+        @case('check_order') 
+            <form action="https://smoservice.media/api/" method="post">
+                <input type="hidden" name="user_id" value="{{$data['user_id']}}">
+                <input type="hidden" name="api_key" value="{{$data['api_key']}}">
+                <input type="hidden" name="action" value="{{$data['action']}}">
+                <input type="hidden" name="order_id" value="{{$data['order_id']}}">
+                <button type="submit">отправить</button>
+            </form>
+            @break
+    @endswitch
 </div>
 @endif
+
--- src/Smoservice.php
+++ src/Smoservice.php
@@ -5,24 +5,24 @@ class Smoservice{
 
     public static function getBalance(array $params){
 
-        return view("smoservice::form",compact('params'));
+        return view("Smoservice::form",compact('params'));
 
     }
 
     public static function getService(array $params){
 
-        return view("smoservice::form",compact('params'));
+        return view("Smoservice::form",compact('params'));
 
     }
 
     public static function createOrder(array $params){
        
-        return view("smoservice::form",compact('params'));
+        return view("Smoservice::form",compact('params'));
     }
     
     public static function checkOrder(array $params){
         
-        return view("smoservice::form",compact('params'));
+        return view("Smoservice::form",compact('params'));
 
     }
 }
